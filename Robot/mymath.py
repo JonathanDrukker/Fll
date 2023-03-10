b86f34606f6efaa1756f54dae99f86faa1c30acc
Jonathan <jondrukker@gmail.com>
2023-03-10 17:19:51 +0200

    Created mean func

---
+++ Robot/mymath.py
@@ -0,0 +1,9 @@
+def mean(*args: float) -> float:
+    """
+    This function returns the mean of the given numbers.
+
+    Parameters: Numbers
+    Returns: Mean-float
+    """
+
+    return sum(args)/len(args)
