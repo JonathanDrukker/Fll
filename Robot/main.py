f4fb14215ad26065bbd294495397cbb7ab52910a
Jonathan <jondrukker@gmail.com>
2023-03-13 19:46:01 +0200

    Created main.py

--- Robot/main.py
+++ Robot/main.py
@@ -1,38 +1,20 @@
 #!/usr/bin/env pybricks-micropython
 
-from ev3devices import Motor, Gyro
 from pybricks.parameters import Port, Direction
-from math import sin, cos, degrees, pi
+from drivebase import DriveBase
+from parameters import Position
 
 
-lm = Motor(Port.D, Direction.CLOCKWISE)
-rm = Motor(Port.C, Direction.CLOCKWISE)
-gyro = Gyro(Port.S4, Direction.COUNTERCLOCKWISE)
+print("Starting...")
 
-r, d = 8.16/2, 9
-c = 2*pi*r
+drivebase = DriveBase((Port.D, Direction.CLOCKWISE),
+                      (Port.B, Direction.CLOCKWISE),
+                      (Port.S3, Direction.COUNTERCLOCKWISE,
+                      Port.S4, Direction.COUNTERCLOCKWISE),
+                      8.16/2, 9.5, Position(0, 0, 0))
 
-x, y, theata = 0, 0, 0
+drivebase.gyro.resetAngle(0)
+drivebase.lm.resetAngle(0)
+drivebase.rm.resetAngle(0)
 
-past_lm_pos = lm.getRawAngle() / 360
-past_rm_pos = rm.getRawAngle() / 360
-
-while True:
-
-    lm_pos = lm.getRawAngle() / 360
-    rm_pos = rm.getRawAngle() / 360
-
-    delta_lm_pos = lm_pos - past_lm_pos
-    delta_rm_pos = rm_pos - past_rm_pos
-
-    ds = c/2*(delta_rm_pos + delta_lm_pos)
-    dTheata = c/d*(delta_rm_pos - delta_lm_pos)
-
-    theata = theata + dTheata
-    x = x + cos(theata)*ds
-    y = y + sin(theata)*ds
-
-    print("x: ", x, "y: ", y, "theta: ", degrees(theata))
-
-    past_lm_pos = lm_pos
-    past_rm_pos = rm_pos
+print("Done!")
