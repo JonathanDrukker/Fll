53f4a81010a2692bb3a3c05c9f710cbb25c0dee8
Jonathan <jondrukker@gmail.com>
2023-03-22 16:36:07 +0200

    Edited to show multiple lines to show robot path

--- Tools/graph.py
+++ Tools/graph.py
@@ -1,23 +1,20 @@
 import matplotlib.pyplot as plt
-from json import load
 
-try:
-    with open('Tools/Graph/graph.json', 'r') as file:
+print("Starting...")
+with open('Tools/graph.data', 'r') as file:
 
-        data = load(file)
-        keys = list(data.keys())
+    data = eval(file.read())
+    keys = list(data.keys())
 
-        for key in keys:
-            if 'x' in key:
-                num = key[1]
-                plt.plot(data[key], data['y'+num])
+    for key in keys:
+        if 'x' in key:
+            num = key[1]
+            plt.plot(data[key], data['y'+num], label=num)
 
-        plt.plot(data['x'], data['y'])
-        plt.title(data['title'])
-        plt.xlabel(data['xlable'])
-        plt.ylabel(data['ylable'])
+    plt.title(data['title'])
+    plt.xlabel(data['lables'][0])
+    plt.ylabel(data['lables'][1])
 
-    plt.show()
-
-except Exception as e:
-    print(e)
+plt.legend()
+plt.show()
+print("Done!")
