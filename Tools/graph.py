232fbbb2221d4bcde5041e76eb7a9b337a2ef06e
Jonathan <jondrukker@gmail.com>
2023-03-28 12:42:10 +0300

    Made usable as module

--- Tools/graph.py
+++ Tools/graph.py
@@ -1,20 +1,28 @@
 import matplotlib.pyplot as plt
 
-print("Starting...")
-with open('Tools/graph.data', 'r') as file:
 
-    data = eval(file.read())
-    keys = list(data.keys())
+def main():
+    print("Starting...")
+    with open('graph.data', 'r') as file:
 
-    for key in keys:
-        if 'x' in key:
-            num = key[1]
-            plt.plot(data[key], data['y'+num], label=num)
+        data = eval(file.read())
+        keys = list(data.keys())
 
-    plt.title(data['title'])
-    plt.xlabel(data['lables'][0])
-    plt.ylabel(data['lables'][1])
+        for key in keys:
+            if 'x' in key:
+                num = key[1]
+                plt.plot(data[key], data['y'+num], label=num)
 
-plt.legend()
-plt.show()
-print("Done!")
+        if 'title' in data:
+            plt.title(data['title'])
+        if 'lables' in data:
+            plt.xlabel(data['lables'][0])
+            plt.ylabel(data['lables'][1])
+
+    plt.legend()
+    plt.show()
+    print("Done!")
+
+
+if __name__ == '__main__':
+    main()
