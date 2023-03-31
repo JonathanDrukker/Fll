20711814daca697b3cdcb993110f2c8fd7163fa6
Jonathan <jondrukker@gmail.com>
2023-03-31 17:59:37 +0300

    Made usable as function
    Made usable as function by adding data parameter instead of .data file in same directory

--- Tools/graph.py
+++ Tools/graph.py
@@ -1,26 +1,29 @@
 import matplotlib.pyplot as plt
 
 
-def main():
-    print("Starting...")
-    with open('graph.data', 'r') as file:
+def graph(data: dict):
 
-        data = eval(file.read())
-        keys = list(data.keys())
+    keys = list(data.keys())
 
-        for key in keys:
-            if 'x' in key:
-                num = key[1]
-                plt.plot(data[key], data['y'+num], label=num)
+    for key in keys:
+        if 'x' in key:
+            num = key[1]
+            plt.plot(data[key], data['y'+num], label=num)
 
-        if 'title' in data:
-            plt.title(data['title'])
-        if 'lables' in data:
-            plt.xlabel(data['lables'][0])
-            plt.ylabel(data['lables'][1])
+    if 'title' in data:
+        plt.title(data['title'])
+    if 'lables' in data:
+        plt.xlabel(data['lables'][0])
+        plt.ylabel(data['lables'][1])
 
     plt.legend()
     plt.show()
+
+
+def main():
+    print("Starting...")
+    with open('graph.data', 'r') as file:
+        graph(eval(file.read()))
     print("Done!")
 
 
