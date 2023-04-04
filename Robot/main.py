65cda7c6581a780d015dca6bbd4752c571a997e7
Jonathan <jondrukker@gmail.com>
2023-04-04 12:45:33 +0300

    Update main.py
    Made connection to server and more

--- Robot/main.py
+++ Robot/main.py
@@ -1,20 +1,40 @@
 #!/usr/bin/env pybricks-micropython
 
+from pybricks.hubs import EV3Brick
 from pybricks.parameters import Port, Direction
 from drivebase import DriveBase
-from parameters import Position
+from Comms.client import Client
+from Comms.message import Message
+from time import sleep
 
 
 print("Starting...")
 
+ev3 = EV3Brick()
 drivebase = DriveBase((Port.D, Direction.CLOCKWISE),
                       (Port.B, Direction.CLOCKWISE),
                       (Port.S3, Direction.COUNTERCLOCKWISE,
                       Port.S4, Direction.COUNTERCLOCKWISE),
-                      8.16/2, 9.5, Position(0, 0, 0))
+                      8.16/2, 9.5, [0, 0, 0])
 
-drivebase.gyro.resetAngle(0)
-drivebase.lm.resetAngle(0)
-drivebase.rm.resetAngle(0)
 
+with open('Comms/ipAdd', 'r') as f:
+    ip = f.read()
+
+print("Connecting to server... IP: ", ip)
+client = Client(ip, 5000)
+print("Connected to server! IP: ", ip)
+
+with open('Test.waypoints', 'r') as f:
+    data = eval(f.read())
+    drivebase.trackPath(data, 1, 0, True, client)
+
+with drivebase.lock:
+    client.send(Message('showGraph', ''))
+
+drivebase.run_tank(0, 0)
+
+sleep(1)
+with drivebase.lock:
+    client.quit()
 print("Done!")
