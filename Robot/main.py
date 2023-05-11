e228051c5d83573b549098ea662253fe1ec4a192
Jonathan <jondrukker@gmail.com>
2023-05-12 02:50:06 +0300

    Update main.py

--- Robot/main.py
+++ Robot/main.py
@@ -15,8 +15,7 @@ drivebase = DriveBase((Port.D, Direction.CLOCKWISE),
                       (Port.B, Direction.CLOCKWISE),
                       (Port.S3, Direction.COUNTERCLOCKWISE,
                       Port.S4, Direction.COUNTERCLOCKWISE),
-                      8.16/2, 9.5, 0, 0, 0)
-
+                      8.16, 9.5, 0, 0, 0)
 
 with open('Comms/ipAdd', 'r') as f:
     ip = f.read()
@@ -26,13 +25,16 @@ client = Client(ip, 5000)
 print("Connected to server! IP: ", ip)
 
 with open('Test.waypoints', 'r') as f:
-    data = eval(f.read())
-    drivebase.trackPath(data, 0.05, 0.1, True, client)
+    waypoints = eval(f.read())
+
+with open('runtime.log', 'w'): pass
+with open('runtime.log', 'a') as logfile:
+    logfile.write('(')
+    drivebase.trackPath(waypoints, 0.06, 0.1, False, logfile, client)
+    logfile.write(')')
 
 with drivebase.lock:
-    client.send(Message('showGraph', ''))
-
-drivebase.run_tank(0, 0)
+    client.send(Message('show', str((drivebase.wheelDiameter, drivebase.DBM))))
 
 sleep(1)
 with drivebase.lock:
