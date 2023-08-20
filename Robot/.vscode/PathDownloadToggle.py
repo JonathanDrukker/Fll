da07bde98e8e1fefe8bc96b70d3c4bdd63fce1ab
JonathanDrukker <107805247+JonathanDrukker@users.noreply.github.com>
2023-08-20 12:23:01 +0300

    Added different types of upload to EV3
    ** need to change settings path to the setting file path of VSCode
    1. Download and Run
    2. Download without path and Run
    3. Build, Download and Run path

---
+++ Robot/.vscode/PathDownloadToggle.py
@@ -0,0 +1,15 @@
+from json import load, dump
+
+
+settings_path = "C:/Users/jondr/AppData/roaming/Code/User/settings.json"
+
+with open(settings_path, 'r') as f:
+    settings = load(f)
+
+if "ev3devBrowser.download.exclude" not in settings or settings["ev3devBrowser.download.exclude"] == "**/.*":
+    settings["ev3devBrowser.download.exclude"] = "*paths.py*"
+else:
+    settings["ev3devBrowser.download.exclude"] = "**/.*"
+
+with open(settings_path, 'w') as f:
+    dump(settings, f)
