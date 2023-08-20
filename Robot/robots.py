2c2d726d924d71b12ddc1a0a04886572062f6e9d
JonathanDrukker <107805247+JonathanDrukker@users.noreply.github.com>
2023-08-20 12:48:26 +0300

    Create robots.py
    Makes easier to get robot data from outside `Robot/` folder

---
+++ Robot/robots.py
@@ -0,0 +1,22 @@
+from math import pi
+
+
+class RobotCharacteristics:
+    def __init__(self, DBM, wheelRad, width, length, wheelAxis):
+        self.DBM = DBM
+        self.halfDBM = DBM / 2
+        self.wheelRad = wheelRad
+        self.wheelDiameter = wheelRad * 2
+        self.wheelCircumference = wheelRad * 2 * pi
+        self.width = width
+        self.length = length
+        self.wheelAxis = wheelAxis
+        self.WA = wheelAxis
+        self.WA_to_middle = self.length/2 - self.WA
+
+
+class Bruria(RobotCharacteristics):
+    def __init__(self):
+        super().__init__(9.7, 4.075, 13.5, 16.5, 5.8)
+
+robot = Bruria() # noqa
