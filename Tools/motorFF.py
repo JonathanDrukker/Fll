46640e08401a66ad2eec373ed1e2721ef7aaecde
JonathanDrukker <107805247+JonathanDrukker@users.noreply.github.com>
2023-11-25 14:11:59 +0200

    Update motorFF.py

--- Tools/motorFF.py
+++ Tools/motorFF.py
@@ -3,7 +3,8 @@ import numpy as np
 
 print('Starting...')
 
-pathToFile = 'Tools/Data/Logs/rightAnalysis.log'
+pathToFile = 'Data/Analysis/rightAnalysis.log'
+# pathToFile = 'Data/Analysis/leftAnalysis.log' # noqa
 
 with open(pathToFile, 'r') as f:
     data = eval(f.read())
@@ -22,5 +23,5 @@ Ks = max(i for i, x in enumerate(data['velocity']) if x == 0)
 Kv = float("%.5g" % Kv)
 Ka = float("%.5g" % Ka)
 
-print(Ks, Kv, Ka, max(x1))
+print(Ks - 1, Kv, Ka, max(x1))
 print('Done!')
