1517caaa075f737dfe0e5f2fb7ace241d23ff5c4
JonathanDrukker <107805247+JonathanDrukker@users.noreply.github.com>
2023-12-06 15:24:23 +0200

    Update drivebase.py

--- Robot/drivebase.py
+++ Robot/drivebase.py
@@ -14,7 +14,7 @@ class DriveBase:
 
         self.lm = Motor(config.drivebase.motor.left)
         self.rm = Motor(config.drivebase.motor.right)
-        if not hasattr(config.gyro, '2'): self.gyro = Gyro(config.gyro.S1)
+        if not hasattr(config.gyro, 'S2'): self.gyro = Gyro(config.gyro.S1)
         else: self.gyro = DualGyro(config.gyro.S1, config.gyro.S2)
 
         self._wheelRad = micropython.const(config.wheel.radius)
@@ -24,7 +24,7 @@ class DriveBase:
         self._DBM = micropython.const(config.drivebase.DBM)
         self._halfDBM = micropython.const(config.drivebase.halfDBM)
 
-    # @micropython.native
+    @micropython.native
     def run_tank(self, Vl: float, Vr: float, Al: float = 0, Ar: float = 0) -> None:
         """
         Run the drivebase at a given speed and acceleration.
@@ -37,7 +37,7 @@ class DriveBase:
         self.lm.run(Vl, Al)
         self.rm.run(Vr, Ar)
 
-    # @micropython.native
+    @micropython.native
     def run_tankCM(self, Vl: float, Vr: float, Al: float = 0, Ar: float = 0) -> None:
         """
         Run the drivebase at a given speed and acceleration.
@@ -50,8 +50,7 @@ class DriveBase:
         self.lm.run(self.motorSpeed(Vl), self.motorSpeed(Al))
         self.rm.run(self.motorSpeed(Vr), self.motorSpeed(Ar))
 
-    # @micropython.native
-    @thread
+    @micropython.native
     def update(self) -> None:
         """
         PID controller for the motor speeds.
@@ -61,7 +60,7 @@ class DriveBase:
         self.lm.update()
         self.rm.update()
 
-    # @micropython.native
+    @micropython.native
     def stopUpdate(self) -> None:
         """
         Stops the PID controller.
@@ -69,7 +68,7 @@ class DriveBase:
         self.lm.stopUpdate()
         self.rm.stopUpdate()
 
-    # @micropython.native
+    @micropython.native
     def stop(self) -> None:
         """
         Stop the drivebase
@@ -77,7 +76,7 @@ class DriveBase:
         self.lm.stop()
         self.rm.stop()
 
-    # @micropython.native
+    @micropython.native
     def motorSpeed(self, V: float) -> float:
         """
         Convert the speed to motor speed.
@@ -89,7 +88,7 @@ class DriveBase:
         """
         return V/self._wheelCircumference*360
 
-    # @micropython.native
+    @micropython.native
     def getAngle(self) -> [float, float]:
         """
         Get the angle of the motors of the drivebase.
@@ -98,7 +97,7 @@ class DriveBase:
         """
         return self.lm.getAngle(), self.rm.getAngle()
 
-    # @micropython.native
+    @micropython.native
     def getRot(self) -> [float, float]:
         """
         Get the rotation of the motors of the drivebase.
@@ -107,7 +106,7 @@ class DriveBase:
         """
         return self.lm.getRot(), self.rm.getRot()
 
-    # @micropython.native
+    @micropython.native
     def getSpeed(self) -> [float, float]:
         """
         Get the speed of the motors of the drivebase.
@@ -116,7 +115,7 @@ class DriveBase:
         """
         return self.lm.getSpeed(), self.rm.getSpeed()
 
-    # @micropython.native
+    @micropython.native
     def reset(self) -> None:
         """
         Reset the motors and gyro of the drivebase.
