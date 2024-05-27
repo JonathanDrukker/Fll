c2512a058de3911a352e4d03cc45156a3ccc9687
Jonathan Drukker <jondrukker@gmail.com>
2024-05-27 18:43:32 +0300

    Happy Bday path program

--- Robot/main.py
+++ Robot/main.py
@@ -4,12 +4,22 @@ import micropython; micropython.opt_level(3)
 from runner import Runner
 from config import config
 from handler import Handler
+from time import sleep
 
 print("Starting...")
 
 runner = Runner(config())
 handler = Handler(runner)
 
-handler.main()
+# handler.main()
+
+handler.startExit()
+runner.drivebase.update()
+
+runner.path('6', 0.02, 0.7)
+
+sleep(8)
+
+runner.path('7', 0.02, 0.7)
 
 print("Done!")
